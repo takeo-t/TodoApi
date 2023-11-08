0928b5adf139742ad3f2500aa904644a7775ebdd
takeo-t <takeo-t@118satellite.com>
2023-11-08 19:02:15 +0900

    マイグレーション実行

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
-    opt.UseSqlite("Data Source=todoapi.db"));
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("TodoContext")));
+    // opt.UseSqlite("Data Source=todoapi.db"));
 // builder.Services.AddDbContext<TodoContext>(opt =>
 //     opt.UseInMemoryDatabase("TodoList"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
