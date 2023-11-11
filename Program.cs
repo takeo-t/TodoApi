3f5db3fb38d7c7dc8420d0b00bd3eb6ef7288a15
takeo-t <takeo-t@118satellite.com>
2023-11-11 22:07:42 +0900

    環境変数の設定

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Env.Load();
+
 builder.Services.AddControllers();
+
+
+var connectionString = Environment.GetEnvironmentVariable("TodoContext");
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new InvalidOperationException("データベース接続文字列が環境変数に設定されていません。");
+}
+
 builder.Services.AddDbContext<TodoContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("TodoContext")));
+    opt.UseSqlServer(connectionString));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -13,8 +24,7 @@ builder.Services.AddCors();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()|| app.Environment.IsProduction())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
@@ -23,7 +33,6 @@ if (app.Environment.IsDevelopment()|| app.Environment.IsProduction())
 app.UseHttpsRedirection();
 
 app.UseCors(options =>
-
     options.WithOrigins("https://delightful-bush-08e8f4500.4.azurestaticapps.net/")
            .AllowAnyMethod()
            .AllowAnyHeader()
