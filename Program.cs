750ed8de9b8ac67c193e6b1ad784276b4f0bcab0
takeo-t <takeo-t@118satellite.com>
2023-11-21 22:59:45 +0900

    Program.csにコメントを追加

--- Program.cs
+++ Program.cs
@@ -3,25 +3,29 @@ using TodoApi.Models;
 using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
-
+//環境変数のロード
 Env.Load();
 
+//コントローラーの追加
 builder.Services.AddControllers();
 
-
+//データベース接続文字列の設定
 var connectionString = Environment.GetEnvironmentVariable("TodoContext");
 if (string.IsNullOrEmpty(connectionString))
 {
     throw new InvalidOperationException("Database connection string is not set in environment variables.");
 }
-
+//データベースコンテキストの設定:
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseSqlServer(connectionString));
-
+//APIエクスプローラーとSwaggerの追加
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//CORS（クロスオリジンリソース共有）の設定
 builder.Services.AddCors();
 
+//アプリケーションのビルドと構成
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
@@ -33,7 +37,7 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 app.UseHttpsRedirection();
 
 app.UseCors(options =>
-    // options.WithOrigins("https://delightful-bush-08e8f4500.4.azurestaticapps.net/")
+
     options.WithOrigins("http://localhost:3000")
            .AllowAnyMethod()
            .AllowAnyHeader()
