38210332f76146fd349760776293bdf6e9ac823c
takeo-t <takeo-t@118satellite.com>
2023-12-03 21:31:23 +0900

    100文字以内バリデーション機能の追加

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TodoApi.Models;
 
 public class TodoItem
 {
     public long Id { get; set; }
+    [StringLength(100, ErrorMessage = "Title cannot be longer than 100 characters.")]
     public string Title { get; set; } = string.Empty;
+    [StringLength(100, ErrorMessage = "Content cannot be longer than 100 characters.")]
     public string Content { get; set; } = string.Empty;
     public DateTime DateTime { get; set; }
     public TodoStatus Status { get; set; }
